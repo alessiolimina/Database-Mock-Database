1e3964d81f023b967576ccec33264e4cac147622
Alessio Limina <alessio.limina@gmail.com>
2023-04-19 17:06:31 +0200

    Exercise Database Mock Database

---
+++ src/main/java/it/develhopemockdatabase/Database/Mock/Database/DatabaseMockDatabaseApplication.java
@@ -0,0 +1,13 @@
+package it.develhopemockdatabase.Database.Mock.Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMockDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMockDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/develhopemockdatabase/Database/Mock/Database/DatabaseMockDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhopemockdatabase.Database.Mock.Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMockDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
